df6dd7ca1c47090f6bb3523b567a160ab36f843b
Felipe Joglar <felipejoglar@gmail.com>
2023-01-10 18:58:50 +0100

    Create Photo Feed KMP project with support for both Android and iOS

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    id("com.android.library").version("7.3.1").apply(false)
+    kotlin("multiplatform").version("1.7.21").apply(false)
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Photo_Feed"
+include(":photofeed")
