e4013312d106eb0c4a29f393c1fb0836fa1b6785
Felipe Joglar <felipejoglar@gmail.com>
2023-01-11 23:32:12 +0100

    `RemoteFeedLoader` starts URL request on load

--- photofeed/src/commonTest/kotlin/com/asturiancoder/photofeed/RemoteFeedLoaderTests.kt
+++ photofeed/src/commonTest/kotlin/com/asturiancoder/photofeed/RemoteFeedLoaderTests.kt
@@ -1,24 +1,46 @@
 package com.asturiancoder.photofeed
 
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertNull
 
-class RemoteFeedLoader {
+class RemoteFeedLoader(
+    private val url: String,
+    private val client: HttpClient,
+) {
 
+    fun load() {
+        client.get(url = url)
+    }
 }
 
 class HttpClient {
     var requestedUrl: String? = null
 
+    fun get(url: String) {
+        requestedUrl = url
+    }
 }
 
 class RemoteFeedLoaderTests {
 
     @Test
     fun init_doesNotStartRequestFromUrl() {
+        val url = "https://a-url.com"
         val client = HttpClient()
-        RemoteFeedLoader()
+        RemoteFeedLoader(url, client)
 
         assertNull(client.requestedUrl)
+    }
+
+    @Test
+    fun load_startsRequestFromUrl() {
+        val url = "https://a-given-url.com"
+        val client = HttpClient()
+        val sut = RemoteFeedLoader(url, client)
+
+        sut.load()
+
+        assertEquals(url, client.requestedUrl)
     }
 }
