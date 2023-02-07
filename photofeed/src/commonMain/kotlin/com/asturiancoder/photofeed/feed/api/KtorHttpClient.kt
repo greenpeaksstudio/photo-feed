1aa9672b323b8515bbb93928d2e926d9a4afa509
Felipe Joglar <felipejoglar@gmail.com>
2023-02-07 01:04:28 +0100

    Refactor `KtorHttpClient` to improve readability

--- photofeed/src/commonMain/kotlin/com/asturiancoder/photofeed/feed/api/KtorHttpClient.kt
+++ photofeed/src/commonMain/kotlin/com/asturiancoder/photofeed/feed/api/KtorHttpClient.kt
@@ -3,19 +3,16 @@ package com.asturiancoder.photofeed.feed.api
 import io.ktor.client.request.get
 import io.ktor.client.statement.bodyAsText
 import kotlinx.coroutines.runBlocking
+import io.ktor.client.statement.HttpResponse as KtorHttpResponse
 
 internal class KtorHttpClient(
     private val client: io.ktor.client.HttpClient
 ) : HttpClient {
 
     override fun get(url: String): Result<HttpResponse> = runBlocking {
-        try {
-            val response = client.get(url)
-            val httpResponse = HttpResponse(code = response.status.value,
-                response.bodyAsText())
-            return@runBlocking Result.success(httpResponse)
-        } catch (exception: Exception) {
-            return@runBlocking Result.failure(exception)
-        }
+        runCatching { client.get(url).map() }
     }
+
+    private suspend fun KtorHttpResponse.map() =
+        HttpResponse(code = status.value, jsonString = bodyAsText())
 }
