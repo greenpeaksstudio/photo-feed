089a7ec92707ca56195af9809af337a880bc1ac4
Felipe Joglar <felipejoglar@gmail.com>
2023-02-28 16:33:29 +0100

    Update kotlin to 1.8.10, AGP to 7.4.1 & Gradle to 7.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
 plugins {
-    id("com.android.library").version("7.3.1").apply(false)
-    kotlin("multiplatform").version("1.7.21").apply(false)
-    kotlin("plugin.serialization").version("1.8.0").apply(false)
+    val kotlinVersion = "1.8.10"
+
+    id("com.android.library").version("7.4.1").apply(false)
+    kotlin("multiplatform").version(kotlinVersion).apply(false)
+    kotlin("plugin.serialization").version(kotlinVersion).apply(false)
 }
 
 tasks.register("clean", Delete::class) {
