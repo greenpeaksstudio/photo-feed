804019c3d429c405e8d77b456aa071aee6e4d9e7
Felipe Joglar <felipejoglar@gmail.com>
2023-03-02 13:41:45 +0100

    Add `spotless` gradle plugin with `ktlint` configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,25 @@ plugins {
     val kotlinVersion = "1.8.10"
 
     id("com.android.library").version("7.4.1").apply(false)
+    id("com.diffplug.spotless").version("6.16.0").apply(true)
     kotlin("multiplatform").version(kotlinVersion).apply(false)
     kotlin("plugin.serialization").version(kotlinVersion).apply(false)
 }
 
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+configure<com.diffplug.gradle.spotless.SpotlessExtension> {
+    val ktlintVersion = "0.48.2"
+
+    kotlin {
+        target("**/*.kt")
+        targetExclude("$buildDir/**/*.kt")
+        targetExclude("bin/**/*.kt")
+
+        ktlint(ktlintVersion)
+    }
+
+    kotlinGradle {
+        target("*.gradle.kts")
+
+        ktlint(ktlintVersion)
+    }
 }
