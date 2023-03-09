9aec78b42de89b941ebeb5126633ba0a88519be9
Felipe Joglar <felipejoglar@gmail.com>
2023-03-09 18:38:58 +0100

    `LocalFeedLoader.load` fails on retrieval error

--- photofeed/src/commonTest/kotlin/com/asturiancoder/photofeed/feed/cache/LocalFeedLoaderTests.kt
+++ photofeed/src/commonTest/kotlin/com/asturiancoder/photofeed/feed/cache/LocalFeedLoaderTests.kt
@@ -1,13 +1,32 @@
 package com.asturiancoder.photofeed.feed.cache
 
+import com.asturiancoder.photofeed.feed.cache.LocalFeedLoaderTests.FeedStoreSpy.Message.RETRIEVE
+import com.asturiancoder.photofeed.feed.feature.FeedLoader
+import com.asturiancoder.photofeed.feed.feature.FeedPhoto
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 internal class LocalFeedLoader(
-    private val feedStore: FeedStore,
+    private val store: FeedStore,
+) : FeedLoader {
+    override fun load(): Result<List<FeedPhoto>> {
+        try {
+            store.retrieve()
+        } catch (exception: Exception) {
+            return Result.failure(exception)
+        }
+        return Result.failure(Exception())
+    }
+}
+
+data class CachedFeed(
+    val feed: List<FeedPhoto>,
+    val timestamp: Long,
 )
 
-internal interface FeedStore
+internal interface FeedStore {
+    fun retrieve(): CachedFeed?
+}
 
 class LocalFeedLoaderTests {
 
@@ -17,15 +36,50 @@ class LocalFeedLoaderTests {
 
         LocalFeedLoader(store)
 
-        assertEquals(0, store.messages.count())
+        assertEquals(0, store.receivedMessages.count())
+    }
+
+    @Test
+    fun load_requestsCacheRetrieval() {
+        val store = FeedStoreSpy()
+        val sut = LocalFeedLoader(store)
+
+        sut.load()
+
+        assertEquals(listOf(RETRIEVE), store.receivedMessages)
+    }
+
+    @Test
+    fun load_failsOnRetrievalError() {
+        val store = FeedStoreSpy()
+        val sut = LocalFeedLoader(store)
+        val retrievalError = Exception()
+
+        store.completeRetrievalWithError(retrievalError)
+        val receivedResult = sut.load()
+
+        assertEquals(Result.failure(retrievalError), receivedResult)
     }
 
     // region Helpers
 
     private class FeedStoreSpy : FeedStore {
-        val messages = emptyList<Message>()
+        val receivedMessages = mutableListOf<Message>()
 
-        private enum class Message
+        private var retrievalResult: Result<CachedFeed?>? = null
+
+        override fun retrieve(): CachedFeed? {
+            receivedMessages.add(RETRIEVE)
+            return retrievalResult?.getOrThrow()
+        }
+
+        fun completeRetrievalWithError(error: Exception) {
+            retrievalResult = Result.failure(error)
+        }
+
+        enum class Message {
+            RETRIEVE,
+        }
     }
 
     // endregion
