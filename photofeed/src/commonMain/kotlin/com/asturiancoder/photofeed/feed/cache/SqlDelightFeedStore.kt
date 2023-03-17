6e7dfe93f8e4fa928e17b1bf6b015bc35b86d3b1
Felipe Joglar <felipejoglar@gmail.com>
2023-03-17 12:28:28 +0100

    `SqlDelightFeedStore.retrieve` delivers found values on non empty cache

--- photofeed/src/commonMain/kotlin/com/asturiancoder/photofeed/feed/cache/SqlDelightFeedStore.kt
+++ photofeed/src/commonMain/kotlin/com/asturiancoder/photofeed/feed/cache/SqlDelightFeedStore.kt
@@ -1,9 +1,54 @@
 package com.asturiancoder.photofeed.feed.cache
 
+import com.asturiancoder.photofeed.feed.cache.db.PhotoFeedDB
 import com.asturiancoder.photofeed.feed.cache.model.CachedFeed
+import com.asturiancoder.photofeed.feed.cache.model.LocalFeedPhoto
+import com.asturiancoder.photofeed.feed.feature.FeedPhoto
+import com.asturiancoder.photofeed.util.Uuid
 
-class SqlDelightFeedStore {
+class SqlDelightFeedStore(
+    db: PhotoFeedDB,
+) {
+
+    private val queries = db.photoFeedDBQueries
+
     fun retrieve(): CachedFeed? {
-        return null
+        return queries.retrieve()
+            .executeAsList()
+            .map { localFeedCache ->
+                CachedFeed(localFeedCache.feed.toModel(), localFeedCache.timestamp)
+            }.firstOrNull()
     }
+
+    fun insert(feed: List<FeedPhoto>, timestamp: Long) {
+        queries.insert(feed.toLocal(), timestamp)
+    }
+}
+
+private fun List<LocalFeedPhoto>.toModel(): List<FeedPhoto> = mapNotNull { localFeedPhoto ->
+    Uuid.from(localFeedPhoto.id)?.let {
+        FeedPhoto(
+            id = it,
+            description = localFeedPhoto.description,
+            location = localFeedPhoto.location,
+            url = localFeedPhoto.url,
+            likes = localFeedPhoto.likes,
+            author = FeedPhoto.Author(
+                name = localFeedPhoto.authorName,
+                imageUrl = localFeedPhoto.authorImageUrl,
+            ),
+        )
+    }
+}
+
+private fun List<FeedPhoto>.toLocal(): List<LocalFeedPhoto> = map {
+    LocalFeedPhoto(
+        id = it.id.uuidString,
+        description = it.description,
+        location = it.location,
+        url = it.url,
+        likes = it.likes,
+        authorName = it.author.name,
+        authorImageUrl = it.author.imageUrl,
+    )
 }
--- photofeed/src/commonTest/kotlin/com/asturiancoder/photofeed/feed/cache/SqlDelightFeedStoreTests.kt
+++ photofeed/src/commonTest/kotlin/com/asturiancoder/photofeed/feed/cache/SqlDelightFeedStoreTests.kt
@@ -1,22 +1,34 @@
 package com.asturiancoder.photofeed.feed.cache
 
+import com.asturiancoder.photofeed.feed.cache.db.FeedColumnAdapter
+import com.asturiancoder.photofeed.feed.cache.db.LocalFeedCache
+import com.asturiancoder.photofeed.feed.cache.db.PhotoFeedDB
+import com.asturiancoder.photofeed.feed.cache.db.TestSqlDelightDriverFactory
+import com.asturiancoder.photofeed.feed.cache.model.CachedFeed
+import com.asturiancoder.photofeed.feed.feature.FeedPhoto
+import com.asturiancoder.photofeed.util.Uuid
+import kotlinx.datetime.Clock
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertNull
 
 class SqlDelightFeedStoreTests {
 
     @Test
     fun retrieve_deliversEmptyOnEmptyCache() {
-        val sut = SqlDelightFeedStore()
+        val adapter = LocalFeedCache.Adapter(FeedColumnAdapter())
+        val db = PhotoFeedDB(TestSqlDelightDriverFactory.create(), adapter)
+        val sut = SqlDelightFeedStore(db)
 
         val receivedCache = sut.retrieve()
-
         assertNull(receivedCache)
     }
 
     @Test
     fun retrieve_hasNoSideEffectsOnEmptyCache() {
-        val sut = SqlDelightFeedStore()
+        val adapter = LocalFeedCache.Adapter(FeedColumnAdapter())
+        val db = PhotoFeedDB(TestSqlDelightDriverFactory.create(), adapter)
+        val sut = SqlDelightFeedStore(db)
 
         var receivedCache = sut.retrieve()
         assertNull(receivedCache)
@@ -24,4 +36,35 @@ class SqlDelightFeedStoreTests {
         receivedCache = sut.retrieve()
         assertNull(receivedCache)
     }
+
+    @Test
+    fun retrieve_deliversFoundValuesOnNonEmptyCache() {
+        val feed = uniquePhotoFeed()
+        val timestamp = Clock.System.now().epochSeconds
+        val adapter = LocalFeedCache.Adapter(FeedColumnAdapter())
+        val db = PhotoFeedDB(TestSqlDelightDriverFactory.create(), adapter)
+        val sut = SqlDelightFeedStore(db)
+
+        sut.insert(feed, timestamp)
+
+        val receivedCache = sut.retrieve()
+
+        assertEquals(CachedFeed(feed, timestamp), receivedCache)
+    }
+
+    // region Helpers
+
+    private fun uniquePhotoFeed(): List<FeedPhoto> =
+        listOf(uniquePhoto(), uniquePhoto())
+
+    private fun uniquePhoto() = FeedPhoto(
+        id = Uuid(),
+        description = "A description",
+        location = "A location",
+        url = "http://a-url.com",
+        likes = 0,
+        author = FeedPhoto.Author(name = "An author", imageUrl = "https://an-author-url.com"),
+    )
+
+    // endregion
 }
