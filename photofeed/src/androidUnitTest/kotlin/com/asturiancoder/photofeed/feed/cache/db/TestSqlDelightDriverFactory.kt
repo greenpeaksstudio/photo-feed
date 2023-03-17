31c500ab7a5397946cf8bbb3889d42fb7dda432d
Felipe Joglar <felipejoglar@gmail.com>
2023-03-17 12:21:53 +0100

    Add test implementations for DB `SqlDriver` using `expect`/`actual`

--- photofeed/src/androidUnitTest/kotlin/com/asturiancoder/photofeed/feed/cache/db/TestSqlDelightDriverFactory.kt
+++ photofeed/src/androidUnitTest/kotlin/com/asturiancoder/photofeed/feed/cache/db/TestSqlDelightDriverFactory.kt
@@ -3,7 +3,7 @@ package com.asturiancoder.photofeed.feed.cache.db
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
 
-actual class SqlDelightDriverFactory {
+actual object TestSqlDelightDriverFactory {
 
     actual fun create(): SqlDriver {
         return JdbcSqliteDriver(JdbcSqliteDriver.IN_MEMORY).apply {
